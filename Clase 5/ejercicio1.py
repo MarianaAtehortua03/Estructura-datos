7ad58dbc6143494f12e5dc1170f9078260a5bb36
marianaateh2005 <marianaateh2005@gmail.com>
2024-09-07 17:39:04 +0000

    Clase 5

---
+++ Clase 5/ejercicio1.py
@@ -0,0 +1,9 @@
+#EJERCICIO 1
+a=2
+b=3
+suma=0
+while a > 0: #control del ciclo
+    suma=suma+b #hacemos operaciones de suma
+    a=a-1 #reducimos la variable de control a cero
+    
+print(suma)
---
+++ Clase 5/ejercicio1r.py
@@ -0,0 +1,8 @@
+#Ejercicio 1 en Recursion
+
+def multiplicacion (a:int, b:int) -> int:
+    if a == 0:
+        return 0
+    return b + multiplicacion(a - 1, b)
+
+print(multiplicacion(4, 5))
